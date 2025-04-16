57ec0ea87c9740f40ef8f576395143c95404f873
JustisGuin <71509622+JustisGuin@users.noreply.github.com>
2025-04-16 15:30:02 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/<REPO>/',
+  base: '/Project2InitialPrototype/',
   plugins: [vue()],
 })
