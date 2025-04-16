751dfe6c048c50cd1013ea4c19c46d3bc9c22570
JustisGuin <71509622+JustisGuin@users.noreply.github.com>
2025-04-16 16:13:54 -0400

    docs html updated

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/Project2InitialPrototype/', 
-  
+  base: '/Project2InitialPrototype/',
+
+  build: {
+    outDir: 'docs', 
+  },
+
   plugins: [vue()],
 })
