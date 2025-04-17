f9488fdad9b431c3105723d3613d49c5b49a9df4
JustisGuin <71509622+JustisGuin@users.noreply.github.com>
2025-04-17 09:56:43 -0400

    vite config

--- vite.config.js
+++ vite.config.js
@@ -2,15 +2,13 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  base: '/Project2InitialPrototype/',
-
+  publicPath: '/',
   build: {
     outDir: 'docs',
-    rollupOptions: {
-      input: {
-        main: 'index.html',
-      }
+  },
+  plugins: [vue()],
+    scripts: {
+      build : "vite build",
+      preview: "vite preview"
     }
-  }
-  
 })
