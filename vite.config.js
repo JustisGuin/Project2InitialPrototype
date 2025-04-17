247940f4ddd811c6b3d2d04b48c762857a15877a
JustisGuin <71509622+JustisGuin@users.noreply.github.com>
2025-04-17 10:31:04 -0400

    please work

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  publicPath: '/Project2InitialPrototype/',
+  base: '/Project2InitialPrototype/',
   build: {
   },
   plugins: [vue()],
